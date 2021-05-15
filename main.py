342512704afd7e51b591aff48e987cec30f10046
Tim Talashok <t.talashok@gmail.com>
2021-05-15 17:47:32 +0200

    Add some comments on what's going on

--- main.py
+++ main.py
@@ -9,23 +9,29 @@ def newSchema(name):
     schemas[name] = {}
     return schemas[name]
 
-id_gen_type = "id"
+id_type = "id"
 
 def schema_generator(json_dict, schema, prefix=""):
     for k, v in json_dict.items():
         # handle nested dict
         if isinstance(v, dict):
+            # flatten nested dictionary into schema
             newprefix = prefix + k + "_"
             schema_generator(v, schema, newprefix)
         # handle nested list
         elif isinstance(v, list):
+            # create new schema
             newschema = newSchema(k)
-            schema[k] = id_gen_type
-            newschema[id_gen_type] = int
+            # set up references from current schema
+            # and we look up the correct schema by k
+            schema[k] = id_type
+            newschema[id_type] = int
+            # if our new schema is a dictionary, fill the schema
             if isinstance(v[0], dict):
                 schema_generator(v[0], newschema, "")
+            # if it is not a dictionary, it's a list of values
             else:
-                newschema[k] = type(v)
+                newschema["value"] = type(v)
         else:
             schema[prefix + k] = type(v)
 
